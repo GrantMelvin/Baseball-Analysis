102bcec16a0a5d22edb40b3981b67e44ff5a7ae9
Jaxon Bauer <103966964+JaxonBauer@users.noreply.github.com>
2024-03-14 11:31:41 -0400

    added new data fields

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-const client = require('./db.js')
-const csvtojson = require('csvtojson') ;
+const client = require("./db.js");
+const csvtojson = require("csvtojson");
 
 async function run() {
-  try {                                                     // Connects to the server
+  try {
+    // Connects to the server
     await client.connect();
 
     // Collection that we are inserting into
-    var collection = client.db('Baseball-Database').collection('Baseball-Collection');
+    // Use Test collection to mess around with new data
+    var collection = client
+      .db("Baseball-Database")
+      .collection("Baseball-Collection");
 
     // Location of the data we want to insert
-    const fileName = './pitcher_data/aaron-nola.csv';
+    const fileName = "./pitcher_data/aaron-nola.csv";
 
     // Temporary storage of data
     var tempData = [];
-    
+
     // Changes CSV -> JSON and pushes it into tempData
-    await csvtojson().fromFile(fileName).then(source => {
+    await csvtojson()
+      .fromFile(fileName)
+      .then((source) => {
+        //change 1 to source.length to get all of the data
         for (var i = 0; i < 1; i++) {
-            var oneRow = {
-                pitch_type: source[i]['pitch_type'],
-                release_speed: source[i]['release_speed'],
-                zone: source[i]['zone'],
-                release_spin_rate: source[i]['release_spin_rate']
-            };
-            tempData.push(oneRow);
+          var oneRow = {
+            player_name: source[i]["player_name"],
+            game_date: source[i]["game_date"],
+            pitch_name: source[i]["pitch_name"],
+            pitch_type: source[i]["pitch_type"],
+            release_speed: source[i]["release_speed"],
+            release_spin_rate: source[i]["release_spin_rate"],
+            description: source[i]["description"],
+            zone: source[i]["zone"],
+          };
+          tempData.push(oneRow);
         }
-    })
+      });
 
     // Pushes data from tempData -> Collection
-    await collection.insertMany(tempData)
-    }finally {                                              // Closes connection to DB
-        console.log('Imported CSV into database successfully!');
-        await client.close();
-    }
+    await collection.insertMany(tempData);
+  } finally {
+    // Closes connection to DB
+    console.log("Imported CSV into database successfully!");
+    await client.close();
+  }
 }
 
-run()
+run();
